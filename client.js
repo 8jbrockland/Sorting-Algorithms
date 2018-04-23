bca18a5f56cbb44a2f0287e4fce8847dbe9b3989
Mr. Fleming <efleming@stpatrick.org>
2018-04-23 08:30:18 -0500

    simplified array code

--- client.js
+++ client.js
@@ -1,33 +1,22 @@
 'use strict;'
 const Sort = require('./sort.js');
 
-var size = 30000;
-var smList = [1,9,2,8,3,7,4,6,5];
-var mdList = new Int32Array(size);
-var lgList = new Int32Array(size*size);
+//change size of array
+let size = 10;
+let list = new Int32Array(size);
 
-
-for(var a = 0;a<size;a++){
-
-	var num = Math.round(size*Math.random());
-	mdList[a] = num;
+//fill array with random #s
+for(let a = 0;a<size;a++){
+	let num = Math.round(size*Math.random());
+	list[a] = num;
 }
 
-for(var a = 0;a<size;a++){
-	for(var b = 0;b<size;b++){
-		lgList[a*size+b] = Math.round(size*size*Math.random());
-	}
-}
 
-//console.log(mdList);
-//console.log(lgList);
-
-var list = mdList;
-
-//Copy this code inside the logs below to run the code.
 //console.log(Sort().bubble(list));
 //console.log(Sort().selection(list));
 //console.log(Sort().insertion(list));
 //console.log(Sort().merge(list));
 
+//toggle this off to test array build
+//switch out the keyword to test different sorting algorithms
 list = Sort().merge(list);
